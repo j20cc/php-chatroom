69df3930f5e6947e655bd794bd1a3b2b1edc62cb
lukedever <luke_44@foxmail.com>
2018-09-15 10:08:24 +0800

    拖了好久啊😕

--- server.php
+++ server.php
@@ -1,24 +1,31 @@
 <?php
+$server = new swoole_websocket_server("0.0.0.0", 9501);
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
 
-class WebsocketTest {
-    public $server;
-    public $redis;
-
-    public function __construct() {
-        $this->server = new swoole_websocket_server("0.0.0.0", 9501);
-        $this->redis = new redis();
-        
-        $this->server->on('open', function (swoole_websocket_server $server, $request) {
-            echo "server: handshake success with fd{$request->fd}\n";
-        });
-        $this->server->on('message', function (swoole_websocket_server $server, $frame) {
-            echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-            $server->push($frame->fd, "this is server");
-        });
-        $this->server->on('close', function ($ser, $fd) {
-            echo "client {$fd} closed\n";
-        });
-        $this->server->start();
-    }
+function makeName($data)
+{
+    var_dump($data['params']);
 }
-new WebsocketTest();
+
+$server->on('open', function (swoole_websocket_server $server, $request) {
+    echo "server: handshake success with fd{$request->fd}\n";
+});
+
+$server->on('message', function (swoole_websocket_server $server, $frame) {
+    $data = json_decode($frame->data, true);
+    $route = $data['route'];
+    $params = $data['params'];
+    var_dump($data);
+    if (function_exists($route)) {
+        call_user_func($route, compact('server', 'frame', 'params'));
+    } else {
+        $server->push($frame->fd, "no such route");
+    }
+});
+
+$server->on('close', function ($server, $fd) {
+    echo "client {$fd} closed\n";
+});
+
+$server->start();
